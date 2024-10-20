d7945beb76b1b3007b4ee89b8d0b9821a11e761c
Shaon-Biswas <shaonbiswas900@gmail.com>
2024-10-20 14:43:52 +0000

    done

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,40 @@
-console.log("Shaon")
+console.log("Shaon")            // to print anything.....
+
+// declare variables....
+
+const accountId = 12345                 // can't change in future...
+let name = "Shaon"                      // for string....
+let age = 22                            // for number....
+
+
+console.log(accountId)
+console.log(name)
+console.log(age);
+
+console.table([accountId,name,age]);
+
+                                                                        // output will be liked this for console.table().....
+                                                                        /*
+                                                                        ┌─────────┬─────────┐
+                                                                        │ (index) │ Values  │
+                                                                        ├─────────┼─────────┤
+                                                                        │ 0       │ 12345   │
+                                                                        │ 1       │ 'Shaon' │
+                                                                        │ 2       │ 22      │
+
+
+                                                                        */
+
+
+
+let name1 = "Shaon"                      // for string....
+let age1 = 22    
+let id;
+let flag=true
+let temp=null
+
+console.log(typeof name1);                      // type  string
+console.log(typeof age1);                       // type number
+console.log(typeof id);                         // type undefine
+console.log(typeof flag);                       // type boolean
+console.log(typeof temp);                       // type object (importent.................)
